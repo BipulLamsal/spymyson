e4ce8a0b0db9dcf922962968656c2737849fe1da
Bipul Lamsal <bipullamsal@gmail.com>
2024-05-23 23:25:47 +0545

    Update: production config

--- server.py
+++ server.py
@@ -2,4 +2,5 @@ from app import create_app, config
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host = config.HOST, port = config.PORT, debug= config.DEBUG, threaded=True)
+    # app.run(host = config.HOST, port = config.PORT, debug= config.DEBUG, threaded=True)
+    app.run(host = config.HOST, debug=config.DEBUG, threaded= True)
